95a5282ed4a20293ac686a8c907eb53bedb8e7ce
Ayush Sharma <81407894+Awesome-sharma@users.noreply.github.com>
2022-04-05 23:52:01 +0530

    Update Longest Substring Without Repeating Characters.py

--- Longest Substring Without Repeating Characters.py
+++ Longest Substring Without Repeating Characters.py
@@ -18,11 +18,6 @@
 # Output: 3
 # Explanation: The answer is "wke", with the length of 3.
 # Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
- 
-# Constraints:
-
-# 0 <= s.length <= 5 * 104
-# s consists of English letters, digits, symbols and spaces.
 
 
 import sys
