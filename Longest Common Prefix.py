442d0d101178100a7ee9041fad0e37b6c7fd7772
Ayush Sharma <81407894+Awesome-sharma@users.noreply.github.com>
2022-04-05 23:51:30 +0530

    Update Longest Common Prefix.py

--- Longest Common Prefix.py
+++ Longest Common Prefix.py
@@ -1,9 +1,6 @@
 # Write a function to find the longest common prefix string amongst an array of strings.
 
 # If there is no common prefix, return an empty string "".
-
- 
-
 # Example 1:
 
 # Input: strs = ["flower","flow","flight"]
