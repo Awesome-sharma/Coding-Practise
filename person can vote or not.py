a6dfd1d23bc28a5b1cc42a45415093d3278cc5f5
Ayush Sharma <81407894+Awesome-sharma@users.noreply.github.com>
2022-08-01 01:01:02 +0530

    Create person can vote or not.py

---
+++ person can vote or not.py
@@ -0,0 +1,19 @@
+# it is given that a person can vote if his or her age is 18 or more else they can not vote. WAP for the same.\
+
+
+age = int(input("give age = "))
+
+
+# if age >= 18:
+#     print("Vote")
+# else:
+#     print("can not vote")
+
+
+# -- code reduced
+# result = "vote" if age >= 18 else "can not vote"
+# print(result)
+
+
+# -- code reduced further
+print ("vote") if age >= 18 else print("can not vote")
