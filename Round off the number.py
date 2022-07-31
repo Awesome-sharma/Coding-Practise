479d1b07607a8f09744ff71ba9cd660fada7cdba
Ayush Sharma <81407894+Awesome-sharma@users.noreply.github.com>
2022-08-01 01:55:52 +0530

    Create Round off the number.py

---
+++ Round off the number.py
@@ -0,0 +1,9 @@
+import math
+
+n = float(input("give number = "))
+print("math.ceil = ",math.ceil(n))
+print("math.floor = ",math.floor(n))
+print("round(n) = ",round(n))
+print("round(n,2) = ",round(n,2))
+print("round(n,1) = ",round(n,1))
+print("round(n,3) = ",round(n,3))
