b33808e9655bf73dcf6253de4fed8fa07ae63d61
Ayush Sharma <81407894+Awesome-sharma@users.noreply.github.com>
2022-08-01 13:55:29 +0530

    Update greater between three numbers.py

--- greater between three numbers.py
+++ greater between three numbers.py
@@ -2,6 +2,7 @@ a = int(input("give value a = "))
 b = int(input("give value b = "))
 c = int(input("give value c = "))
 
+# Method 1
 if a == b == c:
   print("all values are equal !!")
 elif a>b>c:
@@ -10,3 +11,9 @@ elif a<b<c:
   print("c is greater")
 else:
   print("b is greater")
+
+# Method 2
+# if a == b == c:
+#   print("all values are equal !!")
+# else:
+#   print(max(a,max(b,c)))
