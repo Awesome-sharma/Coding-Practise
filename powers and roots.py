d468866a74aa307ec4bb51de89cf885a54b6011c
Ayush Sharma <81407894+Awesome-sharma@users.noreply.github.com>
2022-08-01 13:17:23 +0530

    Create powers and roots.py

---
+++ powers and roots.py
@@ -0,0 +1,16 @@
+import math
+a = int(input("give value a = "))
+b = int(input("give value b = "))
+c = int(input("give value c = "))
+
+square = a ** 2
+sqroot = math.sqrt(b)
+cube = c ** 3
+cbroot = c ** (1/3)
+
+print("square = ",square)
+print("sqroot = ",sqroot)
+print("cube = ",cube)
+print("cbroot = ",cbroot)
+
+
