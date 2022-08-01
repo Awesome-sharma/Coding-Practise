4bb8724ca271087f7ef9dd1a56481a02a904a42b
Ayush Sharma <81407894+Awesome-sharma@users.noreply.github.com>
2022-08-01 13:51:02 +0530

    Create greater between three numbers.py

---
+++ greater between three numbers.py
@@ -0,0 +1,12 @@
+a = int(input("give value a = "))
+b = int(input("give value b = "))
+c = int(input("give value c = "))
+
+if a == b == c:
+  print("all values are equal !!")
+elif a>b>c:
+  print("a is greater")
+elif a<b<c:
+  print("c is greater")
+else:
+  print("b is greater")
